d5c6fed9bedf6e1e69da5ae5633d671559886f77
Kandy-Hamisi <hkandy2000@gmail.com>
2023-10-31 22:59:00 +0300

    full frontend for shoe app

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Services from "./Services";
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    SpecialOffer,
+    Subscribe,
+    CustomerReviews,
+    Footer,
+    Services,
+}
